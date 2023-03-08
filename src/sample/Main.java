13a2802cc32fa6b9bd19b32265d62227da7a813d
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-03-08 09:10:49 +0000

    updated test package to test interactivity

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,34 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
-
-public class Main extends Application {
+public class Main extends Application{
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        StackPane root = new StackPane();
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
+    public void start(Stage primaryStage) throws Exception {
+        Button btn1=new Button("Say, Hello World");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("hello world");
+            }
+        });
+        StackPane root=new StackPane();
+        root.getChildren().add(btn1);
+        Scene scene=new Scene(root,600,400);
+        primaryStage.setTitle("First JavaFX Application");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    public static void main(String[] args) {
+    public static void main (String[] args)
+    {
         launch(args);
     }
+
 }
