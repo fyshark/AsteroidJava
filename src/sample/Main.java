57ff3c51ff65ef1e2becdf0dc7e3639a7c2306cd
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-03-08 09:42:42 +0000

    updated test to include scene changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,29 +5,42 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-public class Main extends Application{
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Scene scene1, scene2;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Button btn1=new Button("Say, Hello World");
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage primaryStage) {
 
-            @Override
-            public void handle(ActionEvent arg0) {
-                System.out.println("hello world");
-            }
-        });
-        StackPane root=new StackPane();
-        root.getChildren().add(btn1);
-        Scene scene=new Scene(root,600,400);
-        primaryStage.setTitle("First JavaFX Application");
-        primaryStage.setScene(scene);
+        primaryStage.setTitle("My First JavaFX GUI");
+
+//Scene 1
+        Label label1= new Label("This is the first scene");
+        Button button1= new Button("Go to scene 2");
+        button1.setOnAction(e -> primaryStage.setScene(scene2));
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1= new Scene(layout1, 300, 250);
+
+//Scene 2
+        Label label2= new Label("This is the second scene");
+        Button button2= new Button("Go to scene 1");
+        button2.setOnAction(e -> primaryStage.setScene(scene1));
+        VBox layout2= new VBox(20);
+        layout2.getChildren().addAll(label2, button2);
+        scene2= new Scene(layout2,300,250);
+
+
+        primaryStage.setScene(scene1);
         primaryStage.show();
     }
-    public static void main (String[] args)
-    {
+
+    public static void main(String[] args) {
         launch(args);
     }
 
