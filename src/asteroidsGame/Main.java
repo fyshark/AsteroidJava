9e536f7b1d1acd2ef0774222e90ee94e9317189b
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-03-23 14:37:55 +0000

    resizing game stage remains between scenes

--- src/asteroidsGame/Main.java
+++ src/asteroidsGame/Main.java
@@ -1,42 +1,76 @@
 package asteroidsGame;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 
 public class Main extends Application {
 
-    Scene scene1, scene2;
+    Scene gameScene, pauseScene;
+    double stageWidth, stageHeight;
 
     @Override
     public void start(Stage primaryStage) {
 
         primaryStage.setTitle("Group 10 Asteroids Game");
-
-//Scene 1
-        Label label1= new Label("This is the first scene");
-        Button button1= new Button("Go to scene 2");
-        button1.setOnAction(e -> primaryStage.setScene(scene2));
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1= new Scene(layout1, 300, 250);
-
-//Scene 2
-        Label label2= new Label("This is the second scene");
-        Button button2= new Button("Go to scene 1");
-        button2.setOnAction(e -> primaryStage.setScene(scene1));
-        VBox layout2= new VBox(20);
-        layout2.getChildren().addAll(label2, button2);
-        scene2= new Scene(layout2,300,250);
-
-        primaryStage.setScene(scene1);
         primaryStage.setMaximized(true);
+        Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+        stageWidth = screenSize.getWidth();
+        stageHeight = screenSize.getHeight();
+        primaryStage.setWidth(stageWidth);
+        primaryStage.setHeight(stageHeight);
+
+        primaryStage.widthProperty().addListener((obs, oldVal, newVal) -> {
+            stageWidth = (double) newVal;
+            primaryStage.setWidth(stageWidth);
+        });
+
+        primaryStage.heightProperty().addListener((obs, oldVal, newVal) -> {
+            stageHeight = (double) newVal;
+            primaryStage.setHeight(stageHeight);
+        });
+
+//Game Scene
+        Button pause = new Button("Pause");
+        HBox pauseHBox = new HBox();
+        pauseHBox.setPadding(new Insets(10, 10, 10, 10));
+
+        final Pane spacer = new Pane();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        spacer.setMinSize(10, 1);
+        pause.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+        pauseHBox.getChildren().addAll(spacer, pause);
+        pause.setOnAction(e -> primaryStage.setScene(pauseScene));
+        gameScene= new Scene(pauseHBox, stageWidth, stageHeight);
+
+//Pause Scene
+        Label pauseSceneTitle = new Label("Pause Menu");
+
+        //Will have to make each of these scenes
+        Button resume = new Button("Resume");
+        Button mainMenu = new Button("Main Menu");
+        Button closeGame = new Button("Close Game");
+
+        resume.setOnAction(e -> primaryStage.setScene(gameScene));
+        //Potential option for scene
+        GridPane gridPauseScene = new GridPane();
+        GridPane.setConstraints(pauseSceneTitle, 0, 0);
+        GridPane.setConstraints(resume,0, 1);
+        GridPane.setConstraints(mainMenu, 0, 2);
+        GridPane.setConstraints(closeGame, 0, 3);
+        gridPauseScene.getChildren().addAll(pauseSceneTitle, resume, mainMenu, closeGame);
+        pauseScene = new Scene(gridPauseScene, stageWidth, stageHeight);
+
+
+        //Will have to be changed to main menu when implemented
+        primaryStage.setScene(gameScene);
         primaryStage.show();
     }
 
