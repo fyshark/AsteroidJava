51979bf3a14913782b4ceab26b3035f0bd1d93d2
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2023-03-24 14:20:05 +0000

    Update Main.java

--- src/asteroidsGame/Main.java
+++ src/asteroidsGame/Main.java
@@ -1,15 +1,19 @@
-package asteroidsGame;
+package com.example.asteroidgame;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.scene.layout.*;
-import javafx.geometry.Insets;
-import javafx.geometry.Rectangle2D;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
 
 public class Main extends Application {
 
@@ -37,7 +41,7 @@ public class Main extends Application {
             primaryStage.setHeight(stageHeight);
         });
 
-//Game Scene
+        //Game Scene
         Button pause = new Button("Pause");
         HBox pauseHBox = new HBox();
         pauseHBox.setPadding(new Insets(10, 10, 10, 10));
@@ -50,28 +54,82 @@ public class Main extends Application {
         pause.setOnAction(e -> primaryStage.setScene(pauseScene));
         gameScene= new Scene(pauseHBox, stageWidth, stageHeight);
 
-//Pause Scene
+        // we create int positions X and Y that we will use to create our ship.
+        // when we create a class for ship we call in an x and y position,
+        // by default these positions are going to be dead in the center.
+
+        int playerX, playerY;
+        playerX = (int) (stageWidth/2);
+        playerY = (int)(stageHeight/2);
+
+        // Instantiating a Ship called player that we can manipulate and adding it to the game scene.
+        Ship player = new Ship (playerX,playerY);
+        spacer.getChildren().add(player.getCharacter());
+
+        //Pause Scene
         Label pauseSceneTitle = new Label("Pause Menu");
 
         //Will have to make each of these scenes
         Button resume = new Button("Resume");
         Button mainMenu = new Button("Main Menu");
         Button closeGame = new Button("Close Game");
+        Button restartGame = new Button("Restart Game");
 
-        resume.setOnAction(e -> primaryStage.setScene(gameScene));
+        // Some simple functionality for the buttons
+        // resume will return back to the primary scene (gameScene)
+        // closeGame will close the application/stage for the game
+        // restartGame will restart the application ... not yet built
+        // mainMenu will bring you back to the starting screen... not yet built
+
+        resume.setOnAction(event -> primaryStage.setScene(gameScene));
+        closeGame.setOnAction(event -> primaryStage.close());
+        restartGame.setOnAction(event ->   {
+            player.resetPosition();
+            primaryStage.setScene(gameScene);
+            primaryStage.show();
+        });
+
         //Potential option for scene
         GridPane gridPauseScene = new GridPane();
         GridPane.setConstraints(pauseSceneTitle, 0, 0);
         GridPane.setConstraints(resume,0, 1);
         GridPane.setConstraints(mainMenu, 0, 2);
         GridPane.setConstraints(closeGame, 0, 3);
-        gridPauseScene.getChildren().addAll(pauseSceneTitle, resume, mainMenu, closeGame);
+        GridPane.setConstraints(restartGame, 0, 4);
+        gridPauseScene.getChildren().addAll(pauseSceneTitle, resume, mainMenu, closeGame, restartGame);
         pauseScene = new Scene(gridPauseScene, stageWidth, stageHeight);
-
 
         //Will have to be changed to main menu when implemented
         primaryStage.setScene(gameScene);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                player.move();
+                // update screen to reflect new position
+            }
+        };
+        timer.start();
+
+        gameScene.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case LEFT:
+                    player.turnLeft();
+                    break;
+                case RIGHT:
+                    player.turnRight();
+                    break;
+                case UP:
+                    player.accelerate();
+                    break;
+                case DOWN:
+                    player.decelerate();
+                    break;
+            }
+        });
+
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
