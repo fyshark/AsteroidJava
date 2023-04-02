cf790a6a7c96fffb29606d877501bc1ff6cb6741
Saoirse555 <81315320+Saoirse555@users.noreply.github.com>
2023-04-02 23:47:06 +0100

    Added in function for adding name of player
    Not perfect still sorting out javafx on the computer. Can't run it but supposedly it should run.

--- src/asteroidsGame/MainMenu.java
+++ src/asteroidsGame/MainMenu.java
@@ -11,6 +11,7 @@ import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -19,133 +20,190 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+import javafx.collections.ListChangeListener;
+import javafx.collections.FXCollections;
+
 
 public class MainMenu {
-    Scene mainPageScene;
-
-    MainMenu(Stage primaryStage, Scene gameScene) {
-        int width = (int) Screen.getPrimary().getBounds().getWidth();
-        int height = (int) Screen.getPrimary().getBounds().getHeight();
-
-        // create a stack pane
-        Pane r = new Pane();
-        Pane playGamePane = new Pane();
-        Pane highScoresPane = new Pane();
-        Button backToPause = new Button("Back to Main Menu");
-        backToPause.setOnAction(e -> primaryStage.setScene(mainPageScene));
-
-        playGamePane.getChildren().addAll(new Label("playGame"), backToPause);
-        highScoresPane.getChildren().add(new Label("highScores"));
-
-        // create a label
-        Label gameName = new Label("ASTROIDS");
-        gameName.setFont(Font.font("Lucida Sans Unicode", FontWeight.BOLD, 150));
-        gameName.setTextFill(Color.WHITE);
-
-        r.getChildren().add(gameName);
-        r.setStyle("-fx-background-color: black");
-
-        // create scenes
-        mainPageScene = new Scene(r, width, height);
-        Scene palyGameScene = new Scene(playGamePane, width, height);
-        Scene highScoresScene = new Scene(highScoresPane, width, height);
-
-        // create buttons
-        Button[] buttons = generateButtons(mainPageScene);
-        Button playGame = buttons[0];
-        Button highScores = buttons[1];
-
-        // Leaderboard Scene
-        Label leaderboardTitle = new Label("Leaderboard");
-        ListView<String> leaderboardList = new ListView<>();
-        ObservableList<String> scoresList = FXCollections.observableArrayList(
-                "Player1: 1000",
-                "Player2: 900",
-                "Player3: 800"
-        );
-        leaderboardList.setItems(scoresList);
-
-        backToPause = new Button("Back to Main Menu");
-        backToPause.setOnAction(e -> primaryStage.setScene(mainPageScene));
-
-        VBox leaderboardLayout = new VBox(10);
-        leaderboardLayout.getChildren().addAll(leaderboardTitle, leaderboardList, backToPause);
-        Scene leaderboardScene = new Scene(leaderboardLayout, width, height);
-
-        playGame.setOnAction(e -> {
-            primaryStage.setScene(gameScene);
-        });
-
-        highScores.setOnAction(e -> {
-            primaryStage.setScene(leaderboardScene);
-        });
-
-        r.getChildren().addAll(buttons);
-
-        // set the scene
-        primaryStage.setScene(mainPageScene);
-
-        playGame.setLayoutX(width / 2d);
-        highScores.setLayoutX(width / 2d);
-        gameName.setLayoutX(width / 2d);
+   Scene mainPageScene;
 
 
-        playGame.setLayoutY(height / 2f + 100);
-        highScores.setLayoutY(height / 2f + 180);
-        gameName.setLayoutY(height / 2f - 100);
+   MainMenu(Stage primaryStage, Scene gameScene) {
+       int width = (int) Screen.getPrimary().getBounds().getWidth();
+       int height = (int) Screen.getPrimary().getBounds().getHeight();
 
-        mainPageScene.widthProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            playGame.setLayoutX(newValue.doubleValue() / 2 - (playGame.widthProperty().getValue() / 2));
-            highScores.setLayoutX(newValue.doubleValue() / 2 - (highScores.widthProperty().getValue() / 2));
-            gameName.setLayoutX(newValue.doubleValue() / 2 - (gameName.widthProperty().getValue() / 2));
-        });
-        mainPageScene.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-            playGame.setLayoutY(newValue.doubleValue() / 2 - (playGame.heightProperty().getValue() / 2 - 100));
-            highScores.setLayoutY(newValue.doubleValue() / 2 - (highScores.heightProperty().getValue() / 2 - 180));
-            gameName.setLayoutY(newValue.doubleValue() / 2 - (gameName.heightProperty().getValue() / 2 + 100));
-        });
 
-        primaryStage.show();
-    }
+       // create a stack pane
+       Pane r = new Pane();
+       Pane playGamePane = new Pane();
+       Pane highScoresPane = new Pane();
+       Button backToPause = new Button("Back to Main Menu");
+       backToPause.setOnAction(e -> primaryStage.setScene(mainPageScene));
 
-    private Button[] generateButtons(Scene sc) {
-        // create a button
-        Button playGame = new Button("Play Game");
-        Button highScores = new Button("High Scores");
 
-        playGame.setTextFill(Color.WHITE);
-        highScores.setTextFill(Color.WHITE);
+       playGamePane.getChildren().addAll(new Label("playGame"), backToPause);
+       highScoresPane.getChildren().add(new Label("highScores"));
 
-        playGame.setStyle("-fx-focus-color: transparent; -fx-background-color: #000000; -fx-font-size:40");
-        highScores.setStyle("-fx-focus-color: transparent; -fx-background-color: #000000; -fx-font-size:40");
 
-        playGame.setOnMouseEntered(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                sc.setCursor(Cursor.HAND); //Change cursor to hand
-            }
-        });
+       // create a label
+       Label gameName = new Label("ASTROIDS");
+       gameName.setFont(Font.font("Lucida Sans Unicode", FontWeight.BOLD, 150));
+       gameName.setTextFill(Color.WHITE);
 
-        playGame.setOnMouseExited(new EventHandler() {
-            public void handle(Event event) {
-                sc.setCursor(Cursor.DEFAULT); //Change cursor to default
-            }
-        });
 
-        highScores.setOnMouseEntered(new EventHandler() {
-            @Override
-            public void handle(Event event) {
-                sc.setCursor(Cursor.HAND); //Change cursor to hand
-            }
-        });
+       r.getChildren().add(gameName);
+       r.setStyle("-fx-background-color: black");
 
-        highScores.setOnMouseExited(new EventHandler() {
-            public void handle(Event event) {
-                sc.setCursor(Cursor.DEFAULT); //Change cursor to default
-            }
-        });
 
-        return new Button[]{playGame, highScores};
-    }
+       // create scenes
+       mainPageScene = new Scene(r, width, height);
+       Scene palyGameScene = new Scene(playGamePane, width, height);
+       Scene highScoresScene = new Scene(highScoresPane, width, height);
+
+
+       // create buttons
+       Button[] buttons = generateButtons(mainPageScene);
+       Button playGame = buttons[0];
+       Button highScores = buttons[1];
+
+
+       // Button leaderboardButton
+       Button leaderboardButton = new Button("Leaderboard");
+
+
+
+
+
+
+       //Adding names to Leaderboard
+       //Cannot use Scanner as they don't work with JavaFx.So this is the javafx type of scanner object
+       TextField name=new TextField();
+       //This creates a button to submit the name to the leaderboard
+       Button submitbutton=new Button("Submit");
+
+
+       // Leaderboard Scene
+       Label leaderboardTitle = new Label("Leaderboard");
+       ListView<String> leaderboardList = new ListView<>();
+
+
+       ObservableList<String> scoresList = FXCollections.observableArrayList(
+               "Player1: 1000",
+               "Player2: 900",
+               "Player3: 800"
+       );
+       //This just adds the name to the leaderboard here
+       submitbutton.setOnAction(e->{
+           //This gets the text that has been inputted
+           String names=name.getText();
+       ObservableList.add(names,"1000");
+       });
+       leaderboardList.setItems(scoresList);
+
+
+       backToPause = new Button("Back to Main Menu");
+       backToPause.setOnAction(e -> primaryStage.setScene(mainPageScene));
+
+
+       VBox leaderboardLayout = new VBox(10);
+       leaderboardLayout.getChildren().addAll(leaderboardTitle, leaderboardList, backToPause);
+       Scene leaderboardScene = new Scene(leaderboardLayout, width, height);
+
+
+       playGame.setOnAction(e -> {
+           primaryStage.setScene(gameScene);
+       });
+
+
+       highScores.setOnAction(e -> {
+           primaryStage.setScene(leaderboardScene);
+       });
+
+
+       r.getChildren().addAll(buttons);
+
+
+       // set the scene
+       primaryStage.setScene(mainPageScene);
+
+
+       playGame.setLayoutX(width / 2d);
+       highScores.setLayoutX(width / 2d);
+       gameName.setLayoutX(width / 2d);
+
+
+
+
+       playGame.setLayoutY(height / 2f + 100);
+       highScores.setLayoutY(height / 2f + 180);
+       gameName.setLayoutY(height / 2f - 100);
+
+
+       mainPageScene.widthProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+           playGame.setLayoutX(newValue.doubleValue() / 2 - (playGame.widthProperty().getValue() / 2));
+           highScores.setLayoutX(newValue.doubleValue() / 2 - (highScores.widthProperty().getValue() / 2));
+           gameName.setLayoutX(newValue.doubleValue() / 2 - (gameName.widthProperty().getValue() / 2));
+       });
+       mainPageScene.heightProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+           playGame.setLayoutY(newValue.doubleValue() / 2 - (playGame.heightProperty().getValue() / 2 - 100));
+           highScores.setLayoutY(newValue.doubleValue() / 2 - (highScores.heightProperty().getValue() / 2 - 180));
+           gameName.setLayoutY(newValue.doubleValue() / 2 - (gameName.heightProperty().getValue() / 2 + 100));
+       });
+
+
+       primaryStage.show();
+   }
+
+
+   private Button[] generateButtons(Scene sc) {
+       // create a button
+       Button playGame = new Button("Play Game");
+       Button highScores = new Button("High Scores");
+
+
+       playGame.setTextFill(Color.WHITE);
+       highScores.setTextFill(Color.WHITE);
+
+
+       playGame.setStyle("-fx-focus-color: transparent; -fx-background-color: #000000; -fx-font-size:40");
+       highScores.setStyle("-fx-focus-color: transparent; -fx-background-color: #000000; -fx-font-size:40");
+
+
+       playGame.setOnMouseEntered(new EventHandler() {
+           @Override
+           public void handle(Event event) {
+               sc.setCursor(Cursor.HAND); //Change cursor to hand
+           }
+       });
+
+
+       playGame.setOnMouseExited(new EventHandler() {
+           public void handle(Event event) {
+               sc.setCursor(Cursor.DEFAULT); //Change cursor to default
+           }
+       });
+
+
+       highScores.setOnMouseEntered(new EventHandler() {
+           @Override
+           public void handle(Event event) {
+               sc.setCursor(Cursor.HAND); //Change cursor to hand
+           }
+       });
+
+
+       highScores.setOnMouseExited(new EventHandler() {
+           public void handle(Event event) {
+               sc.setCursor(Cursor.DEFAULT); //Change cursor to default
+           }
+       });
+
+
+       return new Button[]{playGame, highScores};
+   }
 }
+
 
