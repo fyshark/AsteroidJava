4730014464a437c4239ddfb23568b7fdac800235
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2023-04-10 19:05:27 +0100

    New logic to handle hit detection with bullet, creates new asteroids, and has logic to remove asteroids if less than given size value.

--- src/asteroidsGame/RandomAsteroidGenerator.java
+++ src/asteroidsGame/RandomAsteroidGenerator.java
@@ -2,26 +2,29 @@ package asteroidsGame;
 
 import javafx.scene.shape.Polygon;
 import java.util.Random;
-public class RandomAsteroidGenerator {
-    public Polygon createAsteroid(double size) {
 
+public class RandomAsteroidGenerator {
+
+    public Polygon createAsteroid(double size) {
         Random rnd = new Random();
 
+        // Generate a random number of sides between 12-20
+        int sides = rnd.nextInt(5) + 7;
+
+        // Calculate the angle between each side of the polygon
+        double angle = 2 * Math.PI / sides;
+
         Polygon polygon = new Polygon();
-        double c1 = Math.cos(Math.PI * 2 / 5);
-        double c2 = Math.cos(Math.PI / 5);
-        double s1 = Math.sin(Math.PI * 2 / 5);
-        double s2 = Math.sin(Math.PI * 4 / 5);
+        for (int i = 0; i < sides; i++) {
+            // Calculate the x and y coordinates for each point
+            double x = size * Math.cos(i * angle);
+            double y = size * Math.sin(i * angle);
+            polygon.getPoints().addAll(x, y);
+        }
 
-        polygon.getPoints().addAll(
-                size, 0.0,
-                size * c1, -1 * size * s1,
-                -1 * size * c2, -1 * size * s2,
-                -1 * size * c2, size * s2,
-                size * c1, size * s1);
-
+        // Randomize the distance of each point from the center of the polygon
         for (int i = 0; i < polygon.getPoints().size(); i++) {
-            int change = rnd.nextInt(5) - 2;
+            double change = rnd.nextDouble() * size / 3 * 2 - size / 3;
             polygon.getPoints().set(i, polygon.getPoints().get(i) + change);
         }
 
